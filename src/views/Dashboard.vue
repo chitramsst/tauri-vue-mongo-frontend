c563f65cb73d7c88845606c4f666536e34f061cf
Chitra <chitra@Chitras-MacBook-Pro.local>
2023-05-22 15:18:01 +0530

    sidebar gradiant

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,15 @@
 <template lang="">
   <div class="w-screen mx-auto flex flex-row">
     <div
-      class="inset-y-0 w-64 bg-gray-900 transition-opacity bg-opacity-75 fixed block" v-if="sidebarVisibility"
+      class="inset-y-0 w-64 bg-gray-900 transition-opacity bg-opacity-75 fixed block  bg-gradient-to-bl from-gray-900/75 to-gray-400" v-if="sidebarVisibility"
     >
       <a href="#" class="block p-4 text-white hover:bg-gray-700">Dashboard</a>
+      <a href="#" class="block p-4 text-white hover:bg-gray-700">Category</a>
+      <a href="#" class="block p-4 text-white hover:bg-gray-700">Products</a>
       <a href="#" class="block p-4 text-white hover:bg-gray-700">Logout</a>
     </div>
     <div class="w-full text-black fixed" :class="sidebarVisibility?'ml-64':''">
-      <div class="h-10 bg-gray-900 bg-opacity-75 text-slate-400">
+      <div class="h-20 bg-gray-900 bg-opacity-75 text-slate-400 justify-left items-center flex bg-gradient-to-r from-gray-900/75 to-gray-400">
         <svg
           fill="none"
           stroke="currentColor"
@@ -24,6 +26,9 @@
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           ></path>
         </svg>
+      </div>
+      <div class="h-screen bg-white rounded-lg p-5 w-full">
+       
       </div>
     </div>
   </div>
