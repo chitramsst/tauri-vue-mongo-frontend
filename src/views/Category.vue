738cecc1c71d1fc513493690bd0922dfd584feb7
Chitra <chitra@Chitras-MacBook-Pro.local>
2023-05-22 17:38:00 +0530

    sidebar

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,8 +1,8 @@
 <template lang="">
     <div class="w-full mx-auto flex flex-row">
       <div
-        class="inset-y-0 w-64 bg-gray-900 transition-opacity bg-opacity-75 fixed block bg-gradient-to-bl from-gray-900/75 to-gray-400"
-        v-if="sidebarVisibility" 
+        class="inset-y-0 bg-gray-900 transition-opacity bg-opacity-75 fixed block bg-gradient-to-bl from-gray-900/75 to-gray-400"
+        :class="sidebarVisibility ? 'w-64' : 'w-28'"
       >
         <div
           class="justify-center flex items-center p-4 border-b-[0.01px] border-black"
@@ -15,7 +15,8 @@
         </div>
         <a
           href="#"
-          class="p-4 text-white bg-gray-700 hover:bg-gray-700 font-bold text-md flex flex-row justify-left items-center space-x-5"
+          class="p-4 text-white bg-gray-700 hover:bg-gray-700 font-bold text-md flex flex-row items-center space-x-5"
+          :class="sidebarVisibility ? 'justify-left' : 'justify-center'"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -31,13 +32,14 @@
               d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
             />
           </svg>
-          <span class="flex items-center justify-center h-full"
+          <span class="flex items-center justify-center h-full" v-if="sidebarVisibility"
             >Dashboard</span
           ></a
         >
         <a
         @click="$router.push({name:'category'})"
-          class="p-4 text-white hover:bg-gray-700 font-bold text-md flex flex-row justify-left items-center space-x-5"
+          class="p-4 text-white hover:bg-gray-700 font-bold text-md flex flex-row items-center space-x-5"
+          :class="sidebarVisibility ? 'justify-left' : 'justify-center'"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -53,11 +55,12 @@
               d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
             />
           </svg>
-          <span class="flex items-center justify-center h-full">Category</span></a
+          <span class="flex items-center justify-center h-full" v-if="sidebarVisibility">Category</span></a
         >
         <a
           href="#"
-          class="p-4 text-white hover:bg-gray-700 font-bold text-md flex flex-row justify-left items-center space-x-5"
+          class="p-4 text-white hover:bg-gray-700 font-bold text-md flex flex-row  items-center space-x-5"
+          :class="sidebarVisibility ? 'justify-left' : 'justify-center'"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -73,11 +76,12 @@
               d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
             />
           </svg>
-          <span class="flex items-center justify-center h-full">Product</span></a
+          <span class="flex items-center justify-center h-full" v-if="sidebarVisibility" >Product</span></a
         >
         <a
           href="#"
-          class="p-4 text-white hover:bg-gray-700 font-bold text-md flex flex-row justify-left items-center space-x-5"
+          class="p-4 text-white hover:bg-gray-700 font-bold text-md flex flex-row items-center space-x-5"
+          :class="sidebarVisibility ? 'justify-left' : 'justify-center'"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -93,15 +97,15 @@
               d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
             />
           </svg>
-          <span class="flex items-center justify-center h-full">Logout</span></a
+          <span class="flex items-center justify-center h-full" v-if="sidebarVisibility">Logout</span></a
         >
       </div>
       <div
-        class="w-full text-black "
-        :class="sidebarVisibility ? 'ml-64' : 'ml-30'"
+        class="w-full text-black"
+        :class="sidebarVisibility==true ? 'ml-64' : 'ml-28'"
       >
         <div
-          class="h-20 bg-gray-900 bg-opacity-75 text-slate-400 justify-left items-center flex bg-gradient-to-r from-gray-900/75 to-gray-400 p-5"
+          class="h-20 bg-gray-900 bg-opacity-75 text-slate-400 justify-left items-center flex bg-gradient-to-r from-gray-900/75 to-gray-400 p-5 w-full"
         >
           <svg
             fill="none"
@@ -129,7 +133,7 @@
                 <h1 class="w-full text-left p-5 text-2xl font-bold text-black/50"> Category Management </h1>   
                 <div class="flex flex-col w-full m-5 space-y-5">
                 <lable class="text-black/50 font-medium text-xl"> Name </lable>
-                <input type="text" class="h-15 w-[25%] rounded-xl focus:border-stone-500 focus:ring-0 focus:inline-block text-xl text-black/50" placeholder="nter name"/>        
+                <input type="text" class="h-15 w-[25%] rounded-xl focus:border-stone-500 focus:ring-0 focus:inline-block text-xl text-black/50" placeholder="Enter name"/>        
                 </div>
                  <div class="flex items-center justify-end w-full p-10 space-x-10">
                 <button class="rounded-xl px-10 font-bold text-md bg-blue-800">
