8ca791a9615d346ab60e25c3de9cda650af8d05c
Chitra <chitra@Chitras-MacBook-Pro.local>
2023-05-27 15:38:52 +0530

    home page section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -496,161 +496,17 @@
     <div class="px-36 mt-[200px] py-28">
       <span class="text-black text-[40px] font-bold"> Choose By Brand</span>
       <div class="grid grid-cols-4 my-7 gap-7">
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <svg
-            width="200"
-            height="200"
-            version="1.1"
-            viewBox="0 0 52.917 52.917"
-            xmlns="http://www.w3.org/2000/svg"
-            class="rounded-full h-20 w-20"
-          >
-            <defs>
-              <clipPath id="clipPath831">
-                <path d="M 0,595.28 H 841.89 V 0 H 0 Z" />
-              </clipPath>
-            </defs>
-            <g transform="translate(-9.6659 -106.33)">
-              <g
-                transform="matrix(.35278 0 0 -.35278 -186.45 261.78)"
-                clip-path="url(#clipPath831)"
-              >
-                <g transform="matrix(1.4354 0 0 1.4355 -242.08 -126.56)">
-                  <path
-                    d="m656.46 290.64h-96.563c-2.191 0-3.967 1.776-3.967 3.967v96.563c0 2.191 1.776 3.966 3.967 3.966h96.563c2.191 0 3.967-1.775 3.967-3.966v-96.563c0-2.191-1.776-3.967-3.967-3.967"
-                    fill="#ff6700"
-                    fill-rule="evenodd"
-                  />
-                  <g fill="#fff">
-                    <path
-                      d="m637.87 362.27h-8.221c-0.352 0-0.637-0.28-0.637-0.625v-37.509c0-0.342 0.285-0.622 0.637-0.622h8.221c0.349 0 0.638 0.28 0.638 0.622v37.509c0 0.345-0.289 0.625-0.638 0.625"
-                    />
-                    <path
-                      d="m609.8 362.27h-31.315c-0.352 0-0.635-0.28-0.635-0.625v-37.509c0-0.342 0.283-0.622 0.635-0.622h8.223c0.35 0 0.64 0.28 0.64 0.622v29.439c0 0.34 0.283 0.622 0.635 0.622h17.713c4.981 0 6.395-3.815 6.395-6.276v-23.785c0-0.342 0.286-0.622 0.638-0.622h8.218c0.35 0 0.637 0.28 0.637 0.622v26.609c0 2.195-0.264 5.351-3.096 8.129-2.963 2.9-5.666 3.396-8.688 3.396"
-                    />
-                    <path
-                      d="m604.04 347.33h-8.633c-0.352 0-0.641-0.28-0.641-0.623v-22.574c0-0.341 0.289-0.621 0.641-0.621h8.633c0.349 0 0.635 0.28 0.635 0.621v22.574c0 0.343-0.286 0.623-0.635 0.623"
-                    />
-                  </g>
-                </g>
-              </g>
-            </g>
-          </svg>
-          <div class="flex m-5 justify-between">
-            <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Xiamoi </span>
-              <div class="flex gap-4">
-                <span class="text-[#C5C4C4] text-[17px] font-bold"
-                  >Delivery within 24 hour
-                </span>
-              </div>
-            </div>
+        <div class="bg-white rounded-lg flex items-center justify-center" v-for="item in brandList" :key="item._id">
+          <div class="h-20 w-20 rounded-full">
+            {{
+              $api_url+item.logo
+            }}
+          <!-- <img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" class="h-full w-full rounded-full"/> -->
+          <img :src="$api_url+item.logo" class="h-full w-full rounded-full"/> 
           </div>
-        </div>
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <img
-            src="/src/assets/images/latio.png"
-            class="rounded-full h-20 w-20 object-cover"
-          />
           <div class="flex m-5 justify-between">
             <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Latio </span>
-              <div class="flex gap-4">
-                <span class="text-[#C5C4C4] text-[17px] font-bold"
-                  >Delivery within 24 hour
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <img
-            src="/src/assets/images/piola.png"
-            class="rounded-full h-20 w-20 object-cover"
-          />
-          <div class="flex m-5 justify-between">
-            <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Piola </span>
-              <div class="flex gap-4">
-                <span class="text-[#C5C4C4] text-[17px] font-bold"
-                  >Delivery within 24 hour
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <img
-            src="/src/assets/images/puma.png"
-            class="rounded-full h-20 w-20 object-cover"
-          />
-          <div class="flex m-5 justify-between">
-            <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Puma </span>
-              <div class="flex gap-4">
-                <span class="text-[#C5C4C4] text-[17px] font-bold"
-                  >Delivery within 24 hour
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <img
-            src="/src/assets/images/logo1.png"
-            class="rounded-full h-20 w-20 object-cover"
-          />
-          <div class="flex m-5 justify-between">
-            <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Butilaha </span>
-              <div class="flex gap-4">
-                <span class="text-[#C5C4C4] text-[17px] font-bold"
-                  >Delivery within 24 hour
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <img
-            src="/src/assets/images/logo2.png"
-            class="rounded-full h-20 w-20 object-cover"
-          />
-          <div class="flex m-5 justify-between">
-            <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Lahtuio </span>
-              <div class="flex gap-4">
-                <span class="text-[#C5C4C4] text-[17px] font-bold"
-                  >Delivery within 24 hour
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <img
-            src="/src/assets/images/logo3.jpeg"
-            class="rounded-full h-20 w-20 object-cover"
-          />
-          <div class="flex m-5 justify-between">
-            <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Nauhtiaya </span>
-              <div class="flex gap-4">
-                <span class="text-[#C5C4C4] text-[17px] font-bold"
-                  >Delivery within 24 hour
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="bg-white rounded-lg flex items-center justify-center">
-          <img
-            src="/src/assets/images/logo4.png"
-            class="rounded-full h-20 w-20 object-cover"
-          />
-          <div class="flex m-5 justify-between">
-            <div class="flex flex-col">
-              <span class="text-black text-lg font-bold"> Mialkija </span>
+              <span class="text-black text-lg font-bold"> {{ item.name }} </span>
               <div class="flex gap-4">
                 <span class="text-[#C5C4C4] text-[17px] font-bold"
                   >Delivery within 24 hour
@@ -1936,10 +1792,30 @@ import CategoryModal from "../components/modals/ProductImageModal.vue";
 export default {
   data(){
       return {
-        showLogin : false
+        showLogin : false,
+        brandList : []
       }
   },
-  methods: {},
+  mounted(){
+     this.getBrandsList()
+  },
+  methods: {
+       /* get brands list */
+       async getBrandsList() {
+      try {
+        await this.axios
+          .get(this.$api_url + "home/get-brand-list")
+          .then((response) => {
+            if (response.data.success == true) {
+              this.brandList = response.data.data;
+              console.log(response)
+            }
+          });
+      } catch (e) {
+        console.log("kkk" + e);
+      }
+    },
+  },
   components: {
     CategoryModal,
   },
