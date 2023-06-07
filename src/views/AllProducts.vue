a353c9a08f533aed05748fbb68ce88cb8c564f1d
Chitra <chitra@192.168.1.7>
2023-06-07 12:27:57 +0530

    all products

--- src/views/AllProducts.vue
+++ src/views/AllProducts.vue
@@ -143,7 +143,7 @@
             <span class="text-[#FF9494]"> $985.,35 </span>
           </div>
         </div>
-        <div class="relative" @click="showLogin = !showLogin">
+        <div class="relative">
           <!-- <div
             class="absolute z-10 mt-2 rounded-md bg-white shadow-lg top-5 -right-[80px]"
           >
@@ -172,51 +172,21 @@
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-cart4"
+                class="bi bi-cart4 w-5 h-5"
                 viewBox="0 0 16 16"
               >
                 <path
                   d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"
                 />
               </svg>
-              <!-- <svg
-                fill="#ffffff"
-                version="1.1"
-                id="Capa_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                width="800px"
-                height="800px"
-                viewBox="0 0 495.398 495.398"
-                xml:space="preserve"
-                class="h-5 w-5"
-              >
-                <g>
-                  <g>
-                    <g>
-                      <path
-                        d="M487.083,225.514l-75.08-75.08V63.704c0-15.682-12.708-28.391-28.413-28.391c-15.669,0-28.377,12.709-28.377,28.391
-				v29.941L299.31,37.74c-27.639-27.624-75.694-27.575-103.27,0.05L8.312,225.514c-11.082,11.104-11.082,29.071,0,40.158
-				c11.087,11.101,29.089,11.101,40.172,0l187.71-187.729c6.115-6.083,16.893-6.083,22.976-0.018l187.742,187.747
-				c5.567,5.551,12.825,8.312,20.081,8.312c7.271,0,14.541-2.764,20.091-8.312C498.17,254.586,498.17,236.619,487.083,225.514z"
-                      />
-                      <path
-                        d="M257.561,131.836c-5.454-5.451-14.285-5.451-19.723,0L72.712,296.913c-2.607,2.606-4.085,6.164-4.085,9.877v120.401
-				c0,28.253,22.908,51.16,51.16,51.16h81.754v-126.61h92.299v126.61h81.755c28.251,0,51.159-22.907,51.159-51.159V306.79
-				c0-3.713-1.465-7.271-4.085-9.877L257.561,131.836z"
-                      />
-                    </g>
-                  </g>
-                </g>
-              </svg> -->
-            </span>
+              </span>
           </button>
         </div>
       </div>
     </div>
 
     <!-- popular product -->
-    <div class="w-full bg-white py-20">
+    <div class="w-full bg-white py-20 h-screen">
       <div class="flex justify-between items-center w-full px-36"></div>
       <div class="grid grid-cols-4 gap-7 w-full pt-7 px-36">
         <div
